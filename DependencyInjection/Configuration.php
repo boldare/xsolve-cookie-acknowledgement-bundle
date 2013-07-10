7dab17b803d72a9667672e93fa6d9358ce8ff684
Krzysztof <krzysztof.cholewka@xsolve.pl>
2013-07-10 10:18:31 +0200

    root name

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -21,7 +21,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('xsolve_cookie');
+        $rootNode = $treeBuilder->root('xsolve_cookie_acknowledgement');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
