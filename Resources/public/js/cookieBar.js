6c16d3987c983ee587348638702482182905f9e3
Krzysztof Gzocha <krzysztof.gzocha@xsolve.pl>
2014-12-08 10:56:29 +0100

    Using window.sessionStorage instead of cookies + small tweaks

---
+++ Resources/public/js/cookieBar.js
@@ -0,0 +1,44 @@
+(function () {
+    var COOKIE_BAR = {
+        itemName: 'xsolve.cookie-law-accepted',
+        itemValue: 'accepted',
+        cookieBarDiv: document.getElementById('cookie-law-info-bar'),
+        cookieAcceptButton: document.getElementById('js-cookie-law-close-button'),
+
+        showCookieBar: function () {
+            this.cookieBarDiv.style.display = 'block';
+        },
+
+        hideCookieBar: function () {
+            this.cookieBarDiv.style.display = 'none';
+        },
+
+        shouldShowCookieBar: function () {
+            return window.sessionStorage.getItem(this.itemName) !== this.itemValue;
+        },
+
+        processCookieBar: function () {
+            if (this.shouldShowCookieBar()) {
+                this.showCookieBar();
+            }
+        },
+
+        processCookieAccept: function () {
+            window.sessionStorage.setItem(this.itemName, this.itemValue);
+            this.hideCookieBar();
+        },
+
+        init: function () {
+            var _this = this;
+
+            this.processCookieBar();
+            this.cookieAcceptButton.onclick = function () {
+                _this.processCookieAccept();
+            };
+        }
+    };
+
+    document.addEventListener("DOMContentLoaded", function() {
+        COOKIE_BAR.init();
+    });
+}());
--- Tests/Controller/CookieAcknowledgementControllerTest.php
+++ Tests/Controller/CookieAcknowledgementControllerTest.php
@@ -17,16 +17,4 @@ class CookieAcknowledgementControllerTest extends WebTestCase
         $this->assertEquals(1, $crawler->filter('#cookie-law-info-bar')->count());
 
     }
-
-    public function testIfCookieAcknowledgementBarAppearsIfCookieIsPresent()
-    {
-        $client = static::createClient();
-
-        //set cookies in cookiejar
-        $client->getCookieJar()->set(new Cookie('cookie_law_accepted', '1', time() + 3600 * 24 * 7, '/', null, false, false));
-
-        //check if cookie box is not present
-        $crawler = $client->request('GET', '/');
-        $this->assertEquals(0, $crawler->filter('#cookie-law-info-bar')->count());
-    }
 }
